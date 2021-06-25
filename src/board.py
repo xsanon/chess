ea5686d5b6645c68c218f852d1a6dfcbc756a930
Cathy <xinycath@gmail.com>
2021-06-25 16:29:09 -0400

    moved some functionality into chess class

--- src/board.py
+++ src/board.py
@@ -35,10 +35,6 @@ class Board():
         """
 
         self.board = []
-        self.turn = True
-
-        self.white_ghost_piece = None
-        self.black_ghost_piece = None
 
         # Board set-up
         for i in range(8):
@@ -95,74 +91,3 @@ class Board():
         print(buffer)
 
 
-    def move(self, start, to):
-        """
-        Moves a piece at `start` to `to`. Does nothing if there is no piece at the starting point.
-        Does nothing if the piece at `start` belongs to the wrong color for the current turn.
-        Does nothing if moving the piece from `start` to `to` is not a valid move.
-
-        start : tup
-            Position of a piece to be moved
-
-        to : tup
-            Position of where the piece is to be moved
-        
-        precondition: `start` and `to` are valid positions on the board
-        """
-
-        if self.board[start[0]][start[1]] == None:
-            print("There is no piece to move at the start place")
-            return
-
-        target_piece = self.board[start[0]][start[1]]
-        if self.turn != target_piece.color:
-            print("That's not your piece to move")
-            return
-
-        end_piece = self.board[to[0]][to[1]]
-        is_end_piece = end_piece != None
-
-        # Checks if a player's own piece is at the `to` coordinate
-        if is_end_piece and self.board[start[0]][start[1]].color == end_piece.color:
-            print("There's a piece in the path.")
-            return
-
-        if target_piece.is_valid_move(self, start, to):
-            # Special check for if the move is castling
-            # Board reconfiguration is handled in Piece
-            if target_piece.name == 'K' and abs(start[1] - to[1]) == 2:
-                print("castled")
-                
-                if self.turn and self.black_ghost_piece:
-                    self.board[self.black_ghost_piece[0]][self.black_ghost_piece[1]] = None
-                elif not self.turn and self.white_ghost_piece:
-                    self.board[self.white_ghost_piece[0]][self.white_ghost_piece[1]] = None
-                self.turn = not self.turn
-                return
-                
-            if self.board[to[0]][to[1]]:
-                print(str(self.board[to[0]][to[1]]) + " taken.")
-                # Special logic for ghost piece, deletes the actual pawn that is not in the `to`
-                # coordinate from en passant
-                if self.board[to[0]][to[1]].name == "GP":
-                    if self.turn:
-                        self.board[
-                            self.black_ghost_piece[0] + 1
-                        ][
-                            self.black_ghost_piece[1]
-                        ] = None
-                        self.black_ghost_piece = None
-                    else:
-                        self.board[self.white_ghost_piece[0] - 1][self.black_ghost_piece[1]] = None
-                        self.white_ghost_piece = None
-
-            self.board[to[0]][to[1]] = target_piece
-            self.board[start[0]][start[1]] = None
-            print(str(target_piece) + " moved.")
-
-            if self.turn and self.black_ghost_piece:
-                self.board[self.black_ghost_piece[0]][self.black_ghost_piece[1]] = None
-            elif not self.turn and self.white_ghost_piece:
-                self.board[self.white_ghost_piece[0]][self.white_ghost_piece[1]] = None
-
-            self.turn = not self.turn
--- src/chess.py
+++ src/chess.py
@@ -11,10 +11,32 @@ class Chess():
     -----------
     board : Board
         represents the chess board of the game
+
+    turn : bool
+        True if white's turn
+
+    white_ghost_piece : tup
+        The coordinates of a white ghost piece representing a takeable pawn for en passant
+
+    black_ghost_piece : tup
+        The coordinates of a black ghost piece representing a takeable pawn for en passant
+
+    Methods:
+    --------
+    promote(pos:stup) -> None
+        Promotes a pawn that has reached the other side to another, or the same, piece
+
+    move(start:tup, to:tup) -> None
+        Moves the piece at `start` to `to` if possible. Otherwise, does nothing.
     """
 
     def __init__(self):
         self.board = board.Board()
+
+        self.turn = True
+
+        self.white_ghost_piece = None
+        self.black_ghost_piece = None
 
     def promotion(self, pos):
         pawn = None
@@ -34,6 +56,78 @@ class Chess():
                 elif promote == 'P' or promote == '': 
                     pawn = piece.Pawn(True)
         self.board.board[pos[0]][pos[1]] = pawn 
+
+    def move(self, start, to):
+        """
+        Moves a piece at `start` to `to`. Does nothing if there is no piece at the starting point.
+        Does nothing if the piece at `start` belongs to the wrong color for the current turn.
+        Does nothing if moving the piece from `start` to `to` is not a valid move.
+
+        start : tup
+            Position of a piece to be moved
+
+        to : tup
+            Position of where the piece is to be moved
+        
+        precondition: `start` and `to` are valid positions on the board
+        """
+
+        if self.board.board[start[0]][start[1]] == None:
+            print("There is no piece to move at the start place")
+            return
+
+        target_piece = self.board.board[start[0]][start[1]]
+        if self.turn != target_piece.color:
+            print("That's not your piece to move")
+            return
+
+        end_piece = self.board.board[to[0]][to[1]]
+        is_end_piece = end_piece != None
+
+        # Checks if a player's own piece is at the `to` coordinate
+        if is_end_piece and self.board.board[start[0]][start[1]].color == end_piece.color:
+            print("There's a piece in the path.")
+            return
+
+        if target_piece.is_valid_move(self.board, start, to):
+            # Special check for if the move is castling
+            # Board reconfiguration is handled in Piece
+            if target_piece.name == 'K' and abs(start[1] - to[1]) == 2:
+                print("castled")
+                
+                if self.turn and self.black_ghost_piece:
+                    self.board.board[self.black_ghost_piece[0]][self.black_ghost_piece[1]] = None
+                elif not self.turn and self.white_ghost_piece:
+                    self.board.board[self.white_ghost_piece[0]][self.white_ghost_piece[1]] = None
+                self.turn = not self.turn
+                return
+                
+            if self.board.board[to[0]][to[1]]:
+                print(str(self.board.board[to[0]][to[1]]) + " taken.")
+                # Special logic for ghost piece, deletes the actual pawn that is not in the `to`
+                # coordinate from en passant
+                if self.board.board[to[0]][to[1]].name == "GP":
+                    if self.turn:
+                        self.board.board[
+                            self.black_ghost_piece[0] + 1
+                        ][
+                            self.black_ghost_piece[1]
+                        ] = None
+                        self.black_ghost_piece = None
+                    else:
+                        self.board.board[self.white_ghost_piece[0] - 1][self.black_ghost_piece[1]] = None
+                        self.white_ghost_piece = None
+
+            self.board.board[to[0]][to[1]] = target_piece
+            self.board.board[start[0]][start[1]] = None
+            print(str(target_piece) + " moved.")
+
+            if self.turn and self.black_ghost_piece:
+                self.board.board[self.black_ghost_piece[0]][self.black_ghost_piece[1]] = None
+            elif not self.turn and self.white_ghost_piece:
+                self.board.board[self.white_ghost_piece[0]][self.white_ghost_piece[1]] = None
+
+            self.turn = not self.turn
 
 
 def translate(s):
@@ -71,16 +165,16 @@ if __name__ == "__main__":
         if start == None or to == None:
             continue
 
-        chess.board.move(start, to)
+        chess.move(start, to)
 
         # check for promotion pawns
         i = 0
         while i < 8:
-            if not chess.board.turn and chess.board.board[0][i] != None and \
+            if not chess.turn and chess.board.board[0][i] != None and \
                 chess.board.board[0][i].name == 'P':
                 chess.promotion((0, i))
                 break
-            elif chess.board.turn and chess.board.board[7][i] != None and \
+            elif chess.turn and chess.board.board[7][i] != None and \
                 chess.board.board[7][i].name == 'P':
                 chess.promotion((7, i))
                 break
